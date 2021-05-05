816dc7ce817e49f1dc5480cfff4a0067b2d0d114
RANOJOY <RANOJOY@DESKTOP-IJEOIQ2>
2021-05-05 20:11:42 +0530

    commited

---
+++ AsianPaintSprint/AsianPaintSprint/src/test/java/stepDefinition/ValidLogin.java
@@ -0,0 +1,21 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ValidLogin {
+	@Given("^user is in the login page of the of the application$")
+	public void user_is_in_the_login_page_of_the_of_the_application() throws Exception {
+	}
+
+	@When("^user provides valid (\\d+)$")
+	public void user_provides_valid(int arg1) throws Exception {
+	}
+
+	@Then("^user should successfully view Sign out option$")
+	public void user_should_successfully_view_Sign_out_option() throws Exception {
+	   
+	}
+
+}
